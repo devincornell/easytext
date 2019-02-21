49a9f77b3eca9d7008d15e2321b4291787e89a88
Devin Cornell <devinj.cornell@gmail.com>
2019-02-20 19:46:00 -0800

    Removed some sample code.

--- example_grammar.py
+++ example_grammar.py
@@ -5,46 +5,36 @@ import spacy
 
 if __name__ == '__main__':
     
-    # add easytext pipeline component to new spacy parser
+    # new spacy parser
     nlp = spacy.load('en')
-    #et = EasyTextPipeline()
-    #nlp.add_pipe(et, last=True)
     
-
-
-    #t1 = 'Today I ran over the log with my car in the United States of America. The U.S. went to the store. I went to the hat.'
-    #t2 = 'The United States said they wouldnt get involved. Russia attacked Canada.'
-    #texts = [t1,t2]
-    
+    # open text files and read data
     with open('sampletexts/test.txt','r') as f:
         text = f.read()
     texts = [l for l in text.split('\n') if len(l) > 0]
     names = list(range(len(texts)))
     print(len(texts), 'texts found.')
     
+    # parse all text documents
     docs = list(nlp.pipe(texts))
     
+    # extract entity frequencies
     entlist, entmap = extract_entities(docs)
     print(entlist)
     print()
     
+    # extract prepositional phrases
     preps = extract_prepositions(docs)
     print(preps)
     print()
     
+    # extract noun-verb pairs
     nv = extract_nounverbs(docs)
     print(nv)
     print()
     
+    # extract entity-verb pairs
     ev = extract_entverbs(docs)
     print(ev)
     print()
     
-        
-    #entdf = dict2df(entcts, names)
-    #entdf.append(count_totals(entcts))
-    
-    #prepdf = dict2df(prepcts, names)
-    #nvdf = dict2df(nvcts, names)
-    #evdf = dict2df(evcts, names)
-    #print(entdf)
