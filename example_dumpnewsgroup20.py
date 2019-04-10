8db54533d217624447c0f564aff5438d09c83a7c
Devin Cornell <devinj.cornell@gmail.com>
2019-04-10 12:23:31 -0700

    .

--- example_dumpnewsgroup20.py
+++ example_dumpnewsgroup20.py
@@ -44,11 +44,12 @@ def make_spreadsheet(useN=100, target_fname = 'tmp_example.csv'):
     texts, fnames =  nd['data'][:useN], nd['filenames'][:useN]
     fnames = [fn.split('/')[-1] for fn in fnames]
     
-    df = pd.DataFrame(index=fnames, columns=['text',])
-    for fn,t in zip(fnames,texts):
-        df.loc[fn,'text']
+    df = pd.DataFrame(index=range(len(fnames)), columns=['text',])
+    for i, fn,t in zip(range(len(fnames)),fnames,texts):
+        df.loc[i,'title'] = fn
+        df.loc[i,'text'] = t
      
-    df.to_csv(target_fname)
+    df.to_csv(target_fname, index=False)
     
     print('saved texts to', target_fname)
     
@@ -84,9 +85,11 @@ if __name__ == '__main__':
     if cmd_type == 'spreadsheet':
         make_textfiles(useN,)
         
-    elif cmd_type = 'textfiles':
+    elif cmd_type == 'textfiles':
         make_spreadsheet(useN,)
     
-    elif cmd_type = 'singletext':
+    elif cmd_type == 'singletext':
         make_single_textfile(useN,)
+    else:
+        raise Exception('Enter a valid command! Options: {spreadsheet, textfiles, singletext}')
     
