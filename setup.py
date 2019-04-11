67742d1161cd40ebdd6f100ed43033a8eeab17af
Devin Cornell <devinj.cornell@gmail.com>
2019-04-11 13:33:00 -0700

    Adding new release .tar.gz

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setup(
     url='https://github.com/devincornell/easytext',
     author='Devin J. Cornell',
     author_email='devinj.cornell@gmail.com',
-    download_url = 'https://github.com/user/X/archive/v_01.tar.gz',
+    download_url = 'https://github.com/devincornell/easytext/archive/v0.1.tar.gz',
     keywords=[
         'text analysis', 
         'topic modeling', 
         'parse trees', 
         'educational',
-    ]
+    ],
     install_requires=[
         'sklearn',
         'spacy',
